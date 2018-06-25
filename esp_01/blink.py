997220bb8f9544cf44c3f6298d5756320b497d2c
John Sheehan <jennasyseng@mail.com>
2018-06-24 17:03:20 -0700

    refactored blink to make it execute on import

--- esp_01/blink.py
+++ esp_01/blink.py
@@ -2,17 +2,13 @@ import utime
 from machine import Pin
 
 
-def main():
-    led = Pin(2, Pin.OUT)
-    enabled = False
-    while True:
-        if enabled:
-            led.off()
-        else:
-            led.on()
-        utime.sleep_ms(1000)
-        enabled = not enabled
+led = Pin(2, Pin.OUT)
+enabled = False
+while True:
+    if enabled:
+        led.off()
+    else:
+        led.on()
+    utime.sleep_ms(1000)
+    enabled = not enabled
 
-
-if __name__ == '__main__':
-    main()
