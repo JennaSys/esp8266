4454e169cf147abe177bf91168a7e07586f1233c
John Sheehan <jennasyseng@mail.com>
2018-06-27 10:55:14 -0700

    Added TODOs

--- esp_01/mqtt1.py
+++ esp_01/mqtt1.py
@@ -2,20 +2,31 @@ from umqtt.simple import MQTTClient
 from time import sleep
 import utils
 
-client=None
-
-def rcv(topic,msg):
-    print("{}: {}".format(topic.decode("utf-8"), msg.decode("utf-8")))
-    client.publish('test/stuff','{"rcv":"'+msg.decode("utf-8")+'"}')
-
 client = MQTTClient(utils.get_mac(), '192.168.1.166')
+
+
+def rcv(topic, msg):
+    print("{}: {}".format(topic.decode("utf-8"), msg.decode("utf-8")))
+    client.publish('test/stuff', '{"rcv":"'+msg.decode("utf-8")+'"}')
+
+
 client.set_callback(rcv)
 client.connect()
 client.subscribe('test/junk')
 
+
 def start_loop():
-    while True:
-        client.check_msg()
-        sleep(1)
+    try:
+        while True:
+            client.check_msg()
+            sleep(1)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        client.disconnect()
+
 
 start_loop()
+
+# TODO: add security (user/pwd/cert?)
+# TODO: use config file for server/topic/credentials
