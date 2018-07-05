7a39d5b445cfa8020ec4cb14a2592892477eecf7
John Sheehan <jennasyseng@mail.com>
2018-07-04 17:40:31 -0700

    refactor for clarity

--- esp_01/main.py
+++ esp_01/main.py
@@ -2,9 +2,10 @@ import mqtt2
 from time import sleep
 import network
 
+# Wait for network connection...
 sta_if = network.WLAN(network.STA_IF)
 while not sta_if.isconnected():
     sleep(0.5)
 
-m = mqtt2.Announce()
-m.start()
+announce = mqtt2.Announce()
+announce.start()
