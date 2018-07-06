650454083f46d38be69e915da3c8841be3b1cbd1
John Sheehan <jennasyseng@mail.com>
2018-07-06 00:21:47 -0700

    added option for esp-01 pinout

--- esp_01/blink.py
+++ esp_01/blink.py
@@ -1,8 +1,9 @@
 import utime
 from machine import Pin
 
-
-led = Pin(2, Pin.OUT)
+# LED_PIN = 1  # ESP-01
+LED_PIN = 2  # ESP-12
+led = Pin(LED_PIN, Pin.OUT)
 enabled = False
 while True:
     if enabled:
