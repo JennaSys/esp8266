a830907a8d1c43eff84f7f614d24ec7ac196e3d4
John Sheehan <jennasyseng@mail.com>
2018-07-16 09:51:11 -0700

    Added template for keys.py

---
+++ esp_01/keys_template.py
@@ -0,0 +1,12 @@
+# Network WiFi keys
+ssids = {
+    "Home":dict(
+        SSID="MiCasa",
+        PWD="userpass1234",
+    ),
+
+    "Work":dict(
+        SSID="Staff",
+        PWD="seCretPassw0rdGoEsHeRe",
+    )
+}
