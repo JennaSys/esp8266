2e05f68207088f55e42696a36a2179b3ac4fc1f9
John Sheehan <jennasyseng@mail.com>
2019-01-31 15:17:51 -0800

    Emptied main()

--- esp_01/main.py
+++ esp_01/main.py
@@ -1,11 +1,18 @@
-import mqtt2
-from time import sleep
-import network
+# import mqtt2
+# from time import sleep
+# import network
+#
+# # Wait for network connection...
+# sta_if = network.WLAN(network.STA_IF)
+# while not sta_if.isconnected():
+#     sleep(0.5)
+#
+# announce = mqtt2.Announce()
+# announce.start()
 
-# Wait for network connection...
-sta_if = network.WLAN(network.STA_IF)
-while not sta_if.isconnected():
-    sleep(0.5)
 
-announce = mqtt2.Announce()
-announce.start()
+# from machine import Pin
+# pin_r = Pin(12, Pin.OUT)
+# pin_g = Pin(5, Pin.OUT)
+# pin_b = Pin(4, Pin.OUT)
+
